0e433818bcbf641a0397390b291a601ada0c8531
Matt Sauer <matt598@users.noreply.github.com>
2015-10-12 12:16:54 -0500

    Create Project 1.py

---
+++ python/Project 1.py
@@ -0,0 +1,16 @@
+total = 0;
+x=0;
+while (x <1000):
+    total += x
+    x+=3;
+x=0
+while (x <1000):
+    total += x
+    x+=5
+x=0
+while (x <1000):
+    total -= x
+    x+=15
+x=0;
+print("the total is:")    
+print(total)
