8eceb5d66b4d562971750f5360404a66fa2be9e0
Matt Sauer <matt598@users.noreply.github.com>
2015-10-12 09:52:54 -0500

    Create display.java

---
+++ java/display.java
@@ -0,0 +1,21 @@
+public class Display
+{
+    
+    public Display()
+    {
+        
+    }
+
+    public void showMessage(String message)
+    {
+        System.out.println("" + message);
+    }
+    
+    public String showMessageAndGetResponse(String message)
+    {
+        String response = "";
+        response = "" + 1;
+        
+        return response;
+    }
+}
