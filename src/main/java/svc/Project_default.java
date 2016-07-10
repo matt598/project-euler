7ba559d3088b8117fce5e5806fa4d007e5bf18f7
matt598 <matt598@att.net>
2016-07-09 22:20:34 -0500

    Cleaning up code

--- src/main/java/svc/Project_default.java
+++ src/main/java/svc/Project_default.java
@@ -3,7 +3,7 @@ package svc;
 /**
  * Created by sauer on 7/9/16.
  */
-public class Project_default extends Project{
+class Project_default extends Project{
     public void execute(){
         showMessage("Project Not Found");
     }
